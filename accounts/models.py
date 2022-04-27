54750f20bb064ef1a1460f52cec0427f8c14bb4f
Sebastián Viana <vianasebastian2000@gmail.com>
2022-04-27 15:41:49 -0300

    deleted name from user custom model

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
+# username custom model without name
+
 class UserAccountManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+        if not username:
+            raise ValueError('User must have a username')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+# username custom model with name
+""" class UserAccountManager(BaseUserManager):
     def create_user(self, email, username, name, password=None):
         if not email:
-            raise ValueError('User does not have an email address')
-        
+            raise ValueError('User must have an email address')
+        if not name:
+            raise ValueError('User must have a name')
+        if not username:
+            raise ValueError('User must have a username')
+
         email = self.normalize_email(email)
         user = self.model(email=email, username=username, name=name)
 
@@ -22,4 +53,5 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     objects = UserAccountManager()
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email', 'name']
+    REQUIRED_FIELDS = ['email', 'name'] """
+
